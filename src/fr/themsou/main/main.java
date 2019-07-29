72cc50a9f422e8a10e5760aa5b8bd0b4413e4330
GitUnreal <53376515+GitUnreal@users.noreply.github.com>
2019-07-29 21:22:28 +0200

    Ajout du /sc

--- src/fr/themsou/main/main.java
+++ src/fr/themsou/main/main.java
@@ -113,7 +113,31 @@ public class main extends JavaPlugin implements Listener {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onEnable(){
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[SC] Starting");
+
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[SC] Loading config...");
+
 		
+
+		getConfig().addDefault("Version", "1.0");
+
+		getConfig().addDefault("Prefix", "&7[&4SC&7] ");
+
+		getConfig().addDefault("No Permission", "&4ERROR: You do not have permission to run this command!");
+
+		
+
+		getConfig().options().copyDefaults(true);
+
+		saveConfig();
+
+		
+
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[SC] Successfully loaded config.");
+
+		
+
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[SC] Thank you for using Basic Staff Chat by Unreal !")
 		if(!logblock.exists()){
 			try{
 				logblock.createNewFile();
@@ -280,6 +304,12 @@ public class main extends JavaPlugin implements Listener {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onDisable() {
+		
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[SC] Stopped!");
+
+		
+
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[SC] Thank you for using Basic Staff Chat by Unreal !");
 		Date date = new realDate().getRealDate();
 		
 		main.config.set("stat.list.server.stoptime", date.getDate() + "-" + date.getHours() + ":" + date.getMinutes());
@@ -446,4 +476,153 @@ public class main extends JavaPlugin implements Listener {
  
         return (economy != null);
     }
+}
+public boolean onCommand(CommandSender sender, Command cmd, String list, String args[]) {
+
+		
+
+		// Prefix
+
+		String nonColoredPrefix = getConfig().getString("Prefix");
+
+		String coloredPrefix = ChatColor.translateAlternateColorCodes('&', nonColoredPrefix);
+
+
+
+		if(cmd.getName().equalsIgnoreCase("sc")) {
+
+			
+
+			if(sender.hasPermission("bsc.use")) {
+
+				
+
+				if(args.length == 0) {
+
+					
+
+					sender.sendMessage(coloredPrefix + ChatColor.RED + "ERROR: Not enough arguments!");
+
+					return true;
+
+					
+
+				}
+
+								
+
+				StringBuilder x = new StringBuilder();
+
+				for(int i = 0; i < args.length; i++) {
+
+					
+
+					x.append(args[i] + " ");
+
+					
+
+				}
+
+				
+
+				for(Player all : Bukkit.getOnlinePlayers()){
+
+					if (all.hasPermission("bsc.see")) {
+
+						
+
+						all.sendMessage(coloredPrefix + ChatColor.RED + sender.getName() + ": " + x.toString());
+
+						
+
+					}
+
+				}
+
+				return true;
+
+				
+
+			}
+
+			else {
+
+				
+
+				String nonColoredPermError = getConfig().getString("No Permission");
+
+				String coloredPermError  = ChatColor.translateAlternateColorCodes('&', nonColoredPermError);
+
+				
+
+				sender.sendMessage(coloredPrefix + " " + coloredPermError);
+
+				return true;
+
+				
+
+			}
+
+			
+
+		}
+
+		
+
+		if(cmd.getName().equalsIgnoreCase("screload")) {
+
+			
+
+			if(sender.hasPermission("bsc.reload")) {
+
+				
+
+				sender.sendMessage(coloredPrefix + ChatColor.RED + "Reloading!");
+
+				reloadConfig();
+
+				sender.sendMessage(coloredPrefix + ChatColor.RED + "Reloaded!");
+
+				
+
+				return true;
+
+				// I love how this is the only one of my plugins that can reload their config ;D
+
+				
+
+			}
+
+			else {
+
+				
+
+				String nonColoredPermError = getConfig().getString("No Permission");
+
+				String coloredPermError  = ChatColor.translateAlternateColorCodes('&', nonColoredPermError);
+
+				
+
+				sender.sendMessage(coloredPrefix + " " + coloredPermError);
+
+				return true;
+
+				
+
+			}
+
+			
+
+		}
+
+		
+
+		return true;
+
+		
+
+	}
+
+
+
 }
